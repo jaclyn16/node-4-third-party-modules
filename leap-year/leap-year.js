3310e0083c1ee117772f77e5513fe453537bd0da
jaclyn16 <jgrix16@yahoo.com>
2026-03-03 11:54:03 -0800

    created leap-year npm code

--- leap-year/leap-year.js
+++ leap-year/leap-year.js
@@ -5,3 +5,15 @@ Make an app that determines if a given year is a leap year!
 
 Read the instructions in the README.md file in this folder.
 ------------------------------ */
+
+// import  the moment node module..
+import moment from "moment";
+
+// get user input..
+let year = Number(process.argv[2]);
+
+if (moment([year]).isLeapYear()) {
+    console.log(`${year} is a leap year!`);
+} else {
+    console.log(`${year} is not a leap year!`);
+}
